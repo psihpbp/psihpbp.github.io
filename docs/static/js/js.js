306c6936e0bde69cf2854b1138f7863c97cd72cb
psihpbp <psihpbp@gmail.com>
2018-01-16 21:46:44 -0600

    test

--- docs/static/js/js.js
+++
@@ -1,6 +0,0 @@
-window.onload=function(){
-  document.getElementById("graphic1a").addEventListener("click", function(){
-      var d = document.getElementById("graphic1a");
-      d.className += " graphic1a";
-  });
-}
---
+++ static/js/js.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+  document.getElementById("graphic1a").addEventListener("click", function(){
+      var d = document.getElementById("graphic1a");
+      d.className += " graphic1a";
+  });
+}
