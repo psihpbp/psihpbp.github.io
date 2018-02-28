d5d4a9329b7807b6e942f09cf82f83e4fc4bf9b7
JiaqiGao <jiaqigao12@gmail.com>
2018-02-28 02:16:11 -0600

    updates

--- static/js/js.js
+++ static/js/js.js
@@ -1,6 +1,24 @@
+value = 0;
+
 window.onload=function(){
   document.getElementById("graphic1a").addEventListener("click", function(){
       var d = document.getElementById("graphic1a");
       d.className += " graphic1a";
   });
+  document.getElementById("quiz_start").addEventListener("click", function(){
+      value++;
+      console.log(value);
+  });
+  quiz_init();
+  quiz_start();
+}
+
+function quiz_init(){
+
+
+}
+
+function quiz_start(){
+
+
 }
