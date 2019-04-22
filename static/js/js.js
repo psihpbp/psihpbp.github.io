56f3b0cc75417142c80020166549b6f63a36574f
JiaqiGao <jiaqigao12@gmail.com>
2019-04-21 21:55:43 -0500

    cleaning up files and begin to add new pages

--- static/js/js.js
+++ static/js/js.js
@@ -16,32 +16,23 @@ document.getElementById('menu').addEventListener("click", function(){
     }
 });
 
-document.getElementById('contact').addEventListener("click", function(){
-    console.log("click");
-    // window.open('mailto:psihpbp@gmail.com?subject=Interested&body=body');
+document.getElementById('logo').addEventListener("click", function(){
+    var win = window.open("https://voices.uchicago.edu/ucpsi3/basic-page/", '_blank');
+    win.focus();
 });
-
 
 var drone_video = document.getElementById("drone-video");
 
 $(document).ready( function() {
-
-    // requestAnimationFrame(start);
-    // if (isMobile == false){
     var offsets = document.getElementById('footer').getBoundingClientRect();
     $(window).scroll(function() {
         if ($(window).scrollTop() > 20) {
             drone_video.style.filter = "blur(2px)";
             document.getElementById("scroll").style.display = "none";
-            document.getElementById("learn_more").style.color = "#26272d";
+            document.getElementById("learn_more").style.color = "#10a4b7";
             document.getElementById("learn_more").style.boxShadow = "3px 3px 3px #D3D3D3"
             document.getElementById("learn_more").style.backgroundColor = "white";
             $("#learn_more").addClass("fixed-top");
-
-            // if ($(window).scrollTop() > offsets.top - 50){
-            //     $("#learn_more").removeClass("fixed-top");
-            // }
-
         }else{
             drone_video.style.filter = "none";
             document.getElementById("learn_more").style.color = "white";
@@ -52,20 +43,4 @@ $(document).ready( function() {
         }
     });
 });
-
-
-
-// (function() {
-//     var folded = new OriDomi('.psiofficial', {
-//         vPanels: 6,
-//         ripple: true,
-//         speed: 1200,
-//         shadingIntensity: 2,    // lessen the shading effect
-//         perspective: 800
-//     });
-//
-//     setTimeout(function () {
-//         folded.accordion(50);
-//     }, 1000);
-// }).call(this);
 
